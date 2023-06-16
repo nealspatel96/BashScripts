17f542e4f8c5a9975910867a436cded2484770c6
Neal Patel <nealspatel96@gmail.com>
2023-06-16 16:11:49 -0400

    Add color constants in Colors.sh

---
+++ BashLib/Colors.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DEF_TEXT="\e[30m"
+RED_TEXT="\e[31m"
+GRN_TEXT="\e[32m"
+YEL_TEXT="\e[33m"
+BLU_TEXT="\e[34m"
+PRP_TEXT="\e[35m"
+CYN_TEXT="\e[36m"
+
+RED_HIGHLT="\e[41m"
+GRN_HIGHLT="\e[42m"
+YEL_HIGHLT="\e[43m"
+BLU_HIGHLT="\e[44m"
+PRP_HIGHLT="\e[45m"
+CYN_HIGHLT="\e[46m"
